d7684792826f353cb4a9fd7043c04893f0b414c5
Azar <150106352+Azar-T@users.noreply.github.com>
2024-08-20 20:57:31 +0530

    Add files via upload

---
+++ Challenge1.java
@@ -0,0 +1,11 @@
+public class Challenge1 {
+    public static void main(String[] args) {
+        double a=20.00;
+        double b=80.00;
+        double Total = (a+b)*100.00;
+        double result = Total % 40.00;
+        boolean c = (result == 0.00) ? true :false;
+        System.out.println(c);
+
+    }
+}
---
+++ SecondClass.java
@@ -0,0 +1,23 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("its not an alien");
+        }
+
+        int topScore=100;
+        if(topScore == 100)
+        {
+            System.out.println(" You got a High Score");
+        }
+
+        int SecondTopScore = 60;
+        if(topScore>SecondTopScore && topScore < 100)
+        {
+            System.out.println("print");
+        }
+
+
+    }
+
+}
