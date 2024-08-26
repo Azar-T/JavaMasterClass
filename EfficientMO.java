d83eb360d2ec66b2cfec7009697c5ab4821d9ddd
Azar <150106352+Azar-T@users.noreply.github.com>
2024-08-26 19:29:35 +0530

    Add files via upload

---
+++ EfficientMO.java
@@ -0,0 +1,39 @@
+public class EfficientMO {
+    public static void main(String[] args) {
+
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(-65, 45));
+
+    }
+
+    public static String getDurationString(int seconds) {
+        if(seconds < 0)
+        {
+            return "invalid";
+        }
+        int minutes = seconds / 60;
+       return getDurationString(minutes,seconds);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        if(minutes < 0)
+        {
+            return "invalid";
+        }
+
+        if(seconds < 0 || seconds >59)
+        {
+            return "invalid";
+        }
+        int hours = minutes / 60;
+
+        int remMinutes = minutes % 60;
+
+        int remSeconds = seconds % 60;
+        return hours + "h " + remMinutes + "m " + remSeconds + "s";
+
+    }
+
+
+}
