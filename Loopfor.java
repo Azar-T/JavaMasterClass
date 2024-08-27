5b9f082edfb04aa5ff8958bd5ca21f79c3af52f7
Azar <150106352+Azar-T@users.noreply.github.com>
2024-08-27 23:02:06 +0530

    Update Loopfor.java

--- Loopfor.java
+++ Loopfor.java
@@ -1,14 +1,19 @@
 public class Loopfor {
     public static void main(String[] args) {
-        for (double rate= 2.0 ; rate <= 5.0 ; rate++) {
+        for (double rate = 2.0; rate <= 5.0; rate++) {
 
 
             double interestAmount = calculateInterest(10000, rate);
             System.out.println("10,000 at " + rate + " % interest = " + interestAmount);
         }
+        for (double i = 7.5; i <= 10; i += 0.25) {
+            double interestAmount = calculateInterest(100.00, i);
+            System.out.println("$100.00 at " + i + " % interest = $ " + interestAmount);
+        }
+
     }
-    public static double calculateInterest(double amount, double interestRate)
-    {
-        return (amount * (interestRate/100));
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
     }
 }
