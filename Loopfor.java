47e79dc3c1d71aaec49ca6b65b8f4c8d9f8876cd
Azar <150106352+Azar-T@users.noreply.github.com>
2024-08-27 22:51:09 +0530

    Add files via upload

---
+++ Loopfor.java
@@ -0,0 +1,14 @@
+public class Loopfor {
+    public static void main(String[] args) {
+        for (double rate= 2.0 ; rate <= 5.0 ; rate++) {
+
+
+            double interestAmount = calculateInterest(10000, rate);
+            System.out.println("10,000 at " + rate + " % interest = " + interestAmount);
+        }
+    }
+    public static double calculateInterest(double amount, double interestRate)
+    {
+        return (amount * (interestRate/100));
+    }
+}
