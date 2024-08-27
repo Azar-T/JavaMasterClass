6f3614467c8aa3cd20032ba6c38001d641d0eba3
Azar <150106352+Azar-T@users.noreply.github.com>
2024-08-27 19:12:26 +0530

    Add files via upload

---
+++ SwitchChallenge.java
@@ -0,0 +1,27 @@
+public class SwitchChallenge {
+    public static void main(String[] args) {
+        char ch = 'A';
+        switch(ch)
+        {
+            case 'A':
+                System.out.println("A is able");
+                break;
+            case 'B':
+                System.out.println("B is baker");
+                break;
+            case 'C':
+                System.out.println("c is charlie");
+                break;
+            case 'D':
+                System.out.println("D is dog");
+                break;
+            case 'E':
+                System.out.println("E is easy");
+                break;
+            default:
+                System.out.println(ch + " is not found");
+
+
+        }
+    }
+}
