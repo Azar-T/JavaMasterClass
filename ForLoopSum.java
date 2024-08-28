0dc904c56b987e87826b37c1b21e8485214830ff
Azar <150106352+Azar-T@users.noreply.github.com>
2024-08-28 21:44:32 +0530

    Add files via upload

---
+++ ForLoopSum.java
@@ -0,0 +1,20 @@
+public class ForLoopSum {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        for (int i = 1;  i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                count++;
+                sum += i ;
+                System.out.println("Found a match = " + i);
+
+            }
+            if(count == 5)
+            {
+              break;
+            }
+
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
